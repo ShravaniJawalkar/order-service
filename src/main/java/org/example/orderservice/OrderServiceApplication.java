78f727a4ab7e246de4a671455290bbfcbfe617f9
Shravani_Jawalkar <sjawalkar@corelogic.com>
2025-07-10 10:31:04 +0530

    updates

--- src/main/java/org/example/orderservice/OrderServiceApplication.java
+++ src/main/java/org/example/orderservice/OrderServiceApplication.java
@@ -2,8 +2,11 @@ package org.example.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
